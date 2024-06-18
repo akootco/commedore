8965458ad4c5148abfc91043200f4a216be3e4f6
akoot <akoot@akoot.me>
2024-06-18 16:14:27 -0400

    WE DID IT!!!

--- src/main/kotlin/co/akoot/plugins/commedore/Commedore.kt
+++ src/main/kotlin/co/akoot/plugins/commedore/Commedore.kt
@@ -1,11 +1,13 @@
-package co.akoot.plugins.viveri
+package co.akoot.plugins.commedore
 
+import co.akoot.plugins.bluefox.BlueFox
 import org.bukkit.plugin.java.JavaPlugin
 
-class Viveri : JavaPlugin() {
+class Commedore : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("We are eating good tonight!")
+        BlueFox.trace("hehehe.. - Commedore") // Test that PROVES we can reference BlueFox!!
     }
 
     override fun onDisable() {
